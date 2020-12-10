dd5ca1b7afe8717c81aed8aeefaf64a97b367582
kjablonski08@gmail.com <kjablonski08@gmail.com>
2020-12-09 22:56:46 -0500

    added Comments class to models

--- futures/models.py
+++ futures/models.py
@@ -10,3 +10,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+class Comment(models.Model):
+    body = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
