cf9f7d8e362d00e96f6c96725e11e6b8247320b6
kjablonski08@gmail.com <kjablonski08@gmail.com>
2020-12-13 14:37:44 -0500

    began work on oauth. can login with google from django, but not connected from the frontend

--- futures/views.py
+++ futures/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from rest_framework import viewsets 
-from .models import Comment, Post 
-from .serializers import CommentSerializer, PostSerializer 
+from rest_framework import viewsets
+from .models import Comment, Post
+from .serializers import CommentSerializer, PostSerializer
+
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from users import views
 
 urlpatterns = [
-  path('', include('djoser.urls')),
-  path('', include('djoser.urls.authtoken'))
+    path('', include('djoser.urls')),
+    path('', include('djoser.urls.authtoken'))
 ]
