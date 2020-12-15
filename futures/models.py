6d57e88846a01e8edaa0e5aee2fa54be817f94f7
kjablonski08@gmail.com <kjablonski08@gmail.com>
2020-12-15 16:52:51 -0500

    updated heroku path to frontend in settings.py

--- futures/models.py
+++ futures/models.py
@@ -5,11 +5,17 @@ from django.db import models
 
 class Post(models.Model):
     author = models.ForeignKey(
+<<<<<<< HEAD
         'users.User', on_delete=models.CASCADE)
+=======
+        'users.User', on_delete=models.CASCADE, default=1)
+>>>>>>> deploying to heroku
     title = models.CharField(max_length=100)
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+    image = models.ImageField(
+        upload_to='images/', default='images/default.jpg')
 
     def __str__(self):
         return self.title
--- futures_django/settings.py
+++ futures_django/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ['MODE'] == 'dev' else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -57,6 +57,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -148,6 +149,7 @@ DJOSER = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://building-futures-inc.herokuapp.com/",
 ]
 
 # Internationalization
@@ -168,5 +170,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 AUTH_USER_MODEL = 'users.User'
